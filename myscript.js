14da88d753d4df859be237333106ca98462d8b34
Josh Crisostomo <joshuacr22@gmail.com>
2022-06-24 11:43:47 -0400

    finish resize grid functionality

--- myscript.js
+++ myscript.js
@@ -1,6 +1,8 @@
 let container = document.querySelector(".grid-container");
+let divs = document.getElementsByClassName("grid-div");
 let resetBtn = document.getElementById("reset");
 let randomBtn = document.getElementById("random");
+let gridBtn = document.getElementById("grid");
 
 const getRandomNumber = (maxNum) => {
     return Math.floor(Math.random() * maxNum);
@@ -16,7 +18,13 @@ const getRandomColor = () => {
 
 // function to create divs for grid container
 let createDivs = times => {
-    for(let i = 1; i <= times; i++) {
+    while(container.firstChild) {
+        container.firstChild.remove();
+    }
+    container.style.gridTemplateColumns = `repeat(${times}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${times}, 1fr)`;
+    console.log(container.style)
+    for(let i = 1; i <= Math.pow(times,2); i++) {
         let div = document.createElement("div");
         div.className=(`grid-div`);
         container.appendChild(div);
@@ -46,12 +54,21 @@ let randomColor = () => {
     });
 }
 
+// function to prompt user for grid size
+let getNewGridSize = () => {
+    let gridSize = prompt("Enter a new size for the grid.\nMUST BE UNDER 100.");
+    if (gridSize >= 100 || gridSize <= 0) {
+        alert("You did not enter a valid number for grid size!");
+    } else {
+        createDivs(gridSize);
+    }
+}
 
-
-createDivs(144);
-defaultDraw();
-
+// add event listeners to buttons
 resetBtn.addEventListener('click', resetGrid);
 randomBtn.addEventListener('click', randomColor);
+gridBtn.addEventListener('click', getNewGridSize);
 
+createDivs(12);
+defaultDraw();
 
